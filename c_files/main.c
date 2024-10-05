68d527f4a66eed6dc12f797dd9fad9b221f44945
Daniel <daniel.al-zgul@ucdconnect.ie>
2024-10-05 15:12:15 +0100

    *1000 bug fixed ~~~. Program works as intended now, for 2 packets. 3+ packet queuing delays to be implemented.

--- c_files/main.c
+++ c_files/main.c
@@ -6,15 +6,14 @@
 double packet_delay(int numPackets, int* packets, int numLinks, struct Link *links) {
     double packet_delay = 0;
     // Transmission delay from start-host
-    for (int i = 0; i < numPackets; ++i) {
+    for (int i = 0; i < numPackets-1; ++i) {
         double trans_delay_i = (double) packets[i] / links[0].bitrate;
         packet_delay += trans_delay_i * 1000;
     }
 
-    packet_delay += ((double) links[0].length / links[0].prop_speed) * 1000; // Adding first propogation delay.
 
     // Transmission through routers
-    for (int i = 1; i < numLinks; ++i) {
+    for (int i = 0; i < numLinks; ++i) {
         double prop_delay = ((double) links[i].length / links[i].prop_speed) * 1000;
         double trans_delay = ((double) packets[numPackets-1] / links[i].bitrate) * 1000;
 
@@ -22,11 +21,15 @@ double packet_delay(int numPackets, int* packets, int numLinks, struct Link *lin
         double prev_packet_trans = 0;
 
         // Calculate queueing delay.
-        for (int p = 0; p < numPackets - 1; ++p) {
-            prev_packet_trans += packets[p] / links[i+1].bitrate;
+        if (i != numLinks - 1) {
+            for (int p = 0; p < numPackets - 1; ++p) {
+                prev_packet_trans += ((double) packets[p] / links[i+1].bitrate) * 1000;
+            }
         }
 
-        queueing_delay = (trans_delay > prev_packet_trans) ? 0 : (prev_packet_trans - trans_delay) * 1000;
+        queueing_delay = (trans_delay > prev_packet_trans) ? 0 : (prev_packet_trans - trans_delay);
+
+        printf("QUEUEING DELAY: %f\n", queueing_delay);
         packet_delay += prop_delay + queueing_delay + trans_delay;
     }
     return packet_delay;
